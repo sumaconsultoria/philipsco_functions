b3cc1d2b0456ea78367b2887c17b3a03a7eda22d
Jordan Delgado <jordelquino@gmail.com>
2024-10-28 11:30:31 -0600

    Fixing function

--- netlify/functions/refreshToken.js
+++ netlify/functions/refreshToken.js
@@ -1,35 +1,36 @@
-// netlify/functions/refreshToken.js
+// functions/refreshToken.js
+
 const axios = require('axios');
 
-exports.handler = async (event, context) => {
+exports.handler = async (event) => {
   const clientId = '603047614175354'; // Reemplaza con tu clientId real
   const clientSecret = 'rLgPG6ZtPFn4D4aOUkkjWWPkiAsghp5F'; // Reemplaza con tu clientSecret real
-  const refreshToken = event.body.refreshToken;
+  const refreshToken = JSON.parse(event.body).refreshToken; // Obtener el refreshToken del cuerpo de la solicitud
 
   const url = 'https://api.mercadolibre.com/oauth/token';
-  
   const params = new URLSearchParams();
+
   params.append('grant_type', 'refresh_token');
   params.append('client_id', clientId);
   params.append('client_secret', clientSecret);
   params.append('refresh_token', refreshToken);
 
   try {
-    const response = await axios.post(url, params, {
+    const response = await axios.post(url, params.toString(), {
       headers: {
-        'Accept': 'application/json',
         'Content-Type': 'application/x-www-form-urlencoded',
-      }
+        'Accept': 'application/json',
+      },
     });
-    
+
     return {
       statusCode: 200,
       body: JSON.stringify(response.data),
     };
   } catch (error) {
     return {
-      statusCode: error.response.status || 500,
-      body: JSON.stringify({ error: error.message }),
+      statusCode: error.response.status,
+      body: JSON.stringify({ error: error.response.data }),
     };
   }
 };
